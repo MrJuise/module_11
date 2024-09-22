16c29f0e25abc9bad109a9645a1e7d44d246dc5b
MrJuise <126503964+MrJuise@users.noreply.github.com>
2024-09-22 11:25:10 +0300

    Add files via upload

--- module_11_1.py
+++ module_11_1.py
@@ -1,15 +1,12 @@
 import requests
-from PIL import Image
-
-
 
 page = requests.get("https://google.com")
-
-
 print(f'код ответа: {page.status_code}\n')
 print(f'страница в HTML:\n {page.content}\n')
 print(f'раскодированные данные страницы в текст: \n{page.text}\n')
 # -------------------------------------------------------------------------------
+
+from PIL import Image
 
 img = Image.open('img_1.png')
 print(img.__dict__)
@@ -20,3 +17,17 @@ img = img.rotate(45)
 img = img.resize((800, 600))
 img.save("D:/Python/pythonProject/new_img.png")
 img.show()
+#----------------------------------------------------------------------------------
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 5, 10)
+y = x ** 2
+plt.figure()
+plt.plot(x, y, 'r')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Name schedule')
+plt.show()
+
